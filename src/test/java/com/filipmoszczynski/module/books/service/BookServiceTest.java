f59dd93eab63b1bb98cbe3c868a13aa2b09820b4
Filip M <filip.moszczynski@onet.pl>
2020-11-15 14:11:57 +0100

    BookServiceTest done & green

--- src/test/java/com/filipmoszczynski/module/books/service/BookServiceTest.java
+++ src/test/java/com/filipmoszczynski/module/books/service/BookServiceTest.java
@@ -8,6 +8,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 @RunWith(MockitoJUnitRunner.class)
 public class BookServiceTest {
 
@@ -24,42 +27,56 @@ public class BookServiceTest {
 	@Test
 	public void shouldCallGetOneWhenIdIsValid() {
 		// when
+		bookService.getOne(VALID_BOOK_ID);
 		// then
+		verify(bookRepository).getOne(VALID_BOOK_ID);
 	}
 
 	@Test
 	public void shouldNotCallGetOneWhenIdIsValid() {
 		// when
+		bookService.getOne(NOT_VALID_BOOK_ID);
 		// then
+		verify(bookRepository, times(0)).getOne(NOT_VALID_BOOK_ID);
 	}
 
 	@Test
 	public void shouldCallFindAll() {
 		// when
+		bookService.getAll();
 		// then
+		verify(bookRepository).findAll();
 	}
 
 	@Test
 	public void shouldCallSaveAndFlash() {
 		// when
+		bookService.create(BOOK);
 		// then
+		verify(bookRepository).saveAndFlush(BOOK);
 	}
 
 	@Test
 	public void shouldNotCallSaveAndFlashWhenObjectIsEqualToNull() {
 		// when
+		bookService.create(null);
 		// then
+		verify(bookRepository, times(0)).saveAndFlush(BOOK);
 	}
 
 	@Test
 	public void shouldCallDeleteById() {
 		// when
+		bookService.delete(VALID_BOOK_ID);
 		// then
+		verify(bookRepository).deleteById(VALID_BOOK_ID);
 	}
 
 	@Test
 	public void shouldSaveAndFlashInCaseOfUpdate() {
 		// when
+		bookService.update(VALID_BOOK_ID, BOOK);
 		// then
+		verify(bookRepository).saveAndFlush(BOOK);
 	}
 }

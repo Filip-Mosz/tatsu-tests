58674d5e70700899c0f63cdda912ad381689cfb7
Filip M <filip.moszczynski@onet.pl>
2020-11-15 10:43:21 +0100

    StringTatsuUtilsTest done & green

--- src/test/java/com/filipmoszczynski/module/utils/StringTatsuUtilsTest.java
+++ src/test/java/com/filipmoszczynski/module/utils/StringTatsuUtilsTest.java
@@ -6,33 +6,56 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTatsuUtilsTest {
 
-	@Test
-	public void shouldReturnTrueWhenProductionIsOn() {
-		assertThat(true).isTrue();
-		assertThat("a").isEqualTo("a");
-	}
+    @Test
+    public void shouldReturnTrueWhenProductionIsOn() {
+        assertThat(StringTatsuUtils.isProductionState()).isTrue();
+    }
 
-	@Test
-	public void shouldReturnFalseWhenDevIsOff() {
-	}
+    @Test
+    public void shouldReturnFalseWhenDevIsOff() {
+        assertThat(StringTatsuUtils.isDevState()).isFalse();
+    }
 
-	@Test
-	public void shouldReturnTrueWhenStringIsBlank() {
-	}
+    @Test
+    public void shouldReturnTrueWhenStringIsBlank() {
+        assertThat(StringTatsuUtils.isBlank("")).isTrue();
+        assertThat(StringTatsuUtils.isBlank(null)).isTrue();
+        assertThat(StringTatsuUtils.isBlank("      ")).isTrue();
+    }
 
-	@Test
-	public void shouldReturnFalseWhenStringIsNotBlank() {
-	}
+    @Test
+    public void shouldReturnFalseWhenStringIsNotBlank() {
+        assertThat(StringTatsuUtils.isBlank("thing")).isFalse();
+        assertThat(StringTatsuUtils.isBlank("two things")).isFalse();
+        assertThat(StringTatsuUtils.isBlank("   more  things with spaces  ")).isFalse();
+        assertThat(StringTatsuUtils.isBlank("   s  ")).isFalse();
+    }
 
-	@Test
-	public void shouldReturnFalseWhenEmailIsNotValid() {
-	}
+    @Test
+    public void shouldReturnFalseWhenEmailIsNotValid() {
+        assertThat(StringTatsuUtils.isEmail("")).isFalse();
+        assertThat(StringTatsuUtils.isEmail("to nie jest email")).isFalse();
+        assertThat(StringTatsuUtils.isEmail(null)).isFalse();
+        assertThat(StringTatsuUtils.isEmail("l")).isFalse();
+        assertThat(StringTatsuUtils.isEmail("login@host")).isFalse();
+        assertThat(StringTatsuUtils.isEmail("login.host.domena")).isFalse();
+        assertThat(StringTatsuUtils.isEmail("@host.domena")).isFalse();
+        assertThat(StringTatsuUtils.isEmail("lo gin@host.domena")).isFalse();
+        assertThat(StringTatsuUtils.isEmail("ło gin@host.domęną")).isFalse();
+    }
 
-	@Test
-	public void shouldReturnTrueWhenEmailIsValid() {
-	}
+    @Test
+    public void shouldReturnTrueWhenEmailIsValid() {
+        assertThat(StringTatsuUtils.isEmail("login@host.domena")).isTrue();
+        assertThat(StringTatsuUtils.isEmail("LOGIN@host.domena")).isTrue();
+        assertThat(StringTatsuUtils.isEmail("LOGIN@HOST.domena")).isTrue();
+    }
 
-	@Test
-	public void shouldReturnFullName() {
-	}
+    @Test
+    public void shouldReturnFullName() {
+        assertThat(StringTatsuUtils.getFullName("John", "Doe"))
+                .isEqualTo("John Doe")
+                .isNotNull()
+                .isNotBlank();
+    }
 }
